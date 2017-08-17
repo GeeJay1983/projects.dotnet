dda788c37ed19560bdb974c3e5383403f44f6938
Gert-Jan Jaarsma <gertjan@bencom.nl>
2017-08-17 20:33:41 +0200

    eerst projecten geadd, begin met checklist

---
+++ checklist/Checklist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace checklist
+{
+    public class Checklist 
+    {
+        private List<string> _items;
+        private List<bool> _itemsChecked;
+        
+        public Checklist()
+        {
+            _items = new List<string>();
+            _itemsChecked = new List<bool>();
+        }
+
+        public void AddItem(string item)
+        {
+            _items.Add(item);
+        }
+        
+        public void RemoveItem(int index)
+        {
+            _items.RemoveAt(index);
+        }
+    }
+}
