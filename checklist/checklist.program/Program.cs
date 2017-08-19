27d2c1fc1fa0dee1fe10570fcd17f497b04bd2da
Gert-Jan Jaarsma <gertjan@bencom.nl>
2017-08-19 16:39:12 +0200

    Projecten verplaatst naar dieper niveau. Sln toegevoegd. checklist.program toegevoegd

---
+++ checklist/checklist.program/Program.cs
@@ -0,0 +1,16 @@
+﻿using checklist;
+using System;
+
+namespace checklist.checklist.program
+{
+    public static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var checklist = new Checklist();
+
+            checklist.AddItem("Test item");
+        }
+    }
+}
--- checklist/checklist/Checklist.cs
+++ checklist/checklist/Checklist.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace checklist
 {
@@ -16,11 +17,18 @@ namespace checklist
         public void AddItem(string item)
         {
             _items.Add(item);
+            _itemsChecked.Add(false);
         }
         
         public void RemoveItem(int index)
         {
             _items.RemoveAt(index);
+            _itemsChecked.RemoveAt(index);
+        }
+
+        public void CheckItem(int index)
+        {
+            _itemsChecked[index] = true;
         }
     }
 }
