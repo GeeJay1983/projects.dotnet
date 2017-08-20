fb7eee33cec02ce8defcab1078c147da642bc025
Gert-Jan Jaarsma <gertjan@bencom.nl>
2017-08-20 14:46:35 +0200

    Commentaar toegevoegd, mogelijkheid om via programma items toe te voegen

--- checklist/checklist.program/Program.cs
+++ checklist/checklist.program/Program.cs
@@ -7,18 +7,46 @@ namespace checklist.checklist.program
     {
         internal static void Main(string[] args)
         {
+            // Maak een nieuwe lijst aan
             var checklist = new Checklist();
 
-            var item1 = checklist.AddItem("Toilettas");
-            var item2 = checklist.AddItem("Babyspulletjes");
+            // Zet er toilettas op
+            var toilettasItem = checklist.AddItem("Toilettas");
+            // Zet er luiers op
+            var luiersItem = checklist.AddItem("Luiers");
+            // Zet er nachtluiers op
+            var nachtluiersItem = checklist.AddItem("Nachtluiers");
+            // Zet er flesjes op
+            var flesjesItem = checklist.AddItem("Flesjes");
 
-            checklist.CheckItem(item2.Index);
+            // Vink de luiers af
+            checklist.CheckItem(luiersItem.Index);
+            // Vink de flesjes af
+            checklist.CheckItem(flesjesItem.Index);
 
-            var items = checklist.GetAllItems();
+            var stop = false;
 
-            foreach (var item in items)
-            {          
-                Console.WriteLine(item);
+            while (!stop)
+            {
+            
+                // Vraag alle items van de lijst op
+                var items = checklist.GetAllItems();
+
+                // Toon elk item op het scherm
+                foreach (var item in items)
+                {          
+                    Console.WriteLine(item);
+                }
+
+                Console.Write("Voer iets in om toe te voegen ('stop' om te stoppen): ");
+                var newItem = Console.ReadLine();
+
+                if (newItem == "stop") 
+                {
+                    stop = true;
+                }
+                
+                checklist.AddItem(newItem);
             }
         }
     }
